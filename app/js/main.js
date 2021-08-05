6efeaf159dbf2d7bb8b389b899bf0c717a390064
Nursultan <nmukhamedali@gmail.com>
2021-08-05 11:00:27 +0600

    n

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,29 @@
-(function(){
+(function () {
     const $ = (el) => {
         return document.querySelector(el)
     }
     const $_ = (el) => {
         return document.querySelectorAll(el)
     }
-    try{
-        $('.message__inner .removeIcon').addEventListener('click',function(){
+    try {
+        $('.message__inner .removeIcon').addEventListener('click', function () {
             $('.message_remove_content').style = "transform: scale(1)";
             $('.profile_cards_category_tours_recent').style = "max-height: 100px;overflow-y: hidden";
             $_('.profile_cards_category')[1].style = "overflow-y: unset";
         })
-        $_('.btns button')[1].addEventListener('click',function(e){
+        $_('.btns button')[1].addEventListener('click', function (e) {
             $('.message_remove_content').style = "transform: scale(0)";
             $('.profile_cards_category_tours_recent').style = "max-height: auto;overflow-y: unset";
             $_('.profile_cards_category')[1].style = "overflow-y: scroll";
         });
 
-        const {nav, clickedItem} = {
+        const { nav, clickedItem } = {
             nav: $_('.profile_navbar_settings'),
             clickedItem: {}
         };
         nav.forEach((e) => {
             clickedItem[e] = false
-            e.addEventListener('click',function(e){
+            e.addEventListener('click', function (e) {
                 const transforms = {
                     scale: clickedItem[e] ? "transform: scale(0)" : "transform: scale(1)",
                     rotate: clickedItem[e] ? "transform: rotate(0deg)" : "transform: rotate(180deg)"
@@ -34,9 +34,10 @@
             });
         })
     }
-    catch(e){
+    catch (e) {
         console.log(e);
     }
+})();
 (function () {
     var btn = document.querySelectorAll('.burger-show');
     let burgerOpen = document.querySelector(".burger-menu");
@@ -48,4 +49,4 @@
             clicked = !clicked
         }
     })
-})()
+})();
